e4096a22539736a06552a23018fc4634469544d5
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2025-02-05 21:55:56 +0200

    Add oldPassword , newPassword , confirmNewPassword to editUserDto

--- News.Core/Dtos/EditUserDto.cs
+++ News.Core/Dtos/EditUserDto.cs
@@ -7,9 +7,12 @@ namespace News.Core.Dtos
         public string? FirstName { get; set; }
         public string? LastName { get; set; } 
         public IFormFile? ProfilePicUrl { get; set; }
-        public string? Username { get; set; } 
-        public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; } 
+        public string? Username { get; set; }
+        //public string? Password { get; set; }
+        //public string? ConfirmPassword { get; set; } 
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmNewPassword { get; set; }
         public string? Email { get; set; } 
     }
 }
