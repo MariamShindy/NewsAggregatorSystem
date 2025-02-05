70762521e3da45421aee41a38d536c4b4baf84f2
MariamShindy <125688137+MariamShindy@users.noreply.github.com>
2025-02-05 22:27:18 +0200

    Refactor comments service & controller

---
+++ News.Core/Dtos/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Core.Dtos
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string CreatedAt { get; set; } = string.Empty; 
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public bool IsLocked { get; set; } 
+    }
+}
